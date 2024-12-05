d9b7815a16c0db05a6b4cb59621be080667321c4
kamis <kamilabr.rodrigues@gmail.com>
2024-12-05 09:48:30 -0300

    Aula 02 phyton.

---
+++ AULA DE PHYTON/aula02.py
@@ -0,0 +1,11 @@
+print('bom dia divos')
+
+nome_usuario = 'Kamila' # tipo -> str = string (texto)
+idade_usuario = 19 # tipo -> int = integer (numero inteiro)
+altura_usuario = 1.83 # tipo -> float = ponto flutuante 
+maior_idade = True # tipo -> bool = boolean 
+
+print('Seja bem vinda diva ', nome_usuario)
+print('minha idade é', idade_usuario)
+print('minha altura é', altura_usuario )
+print('eu sou maior de idade?', maior_idade)
